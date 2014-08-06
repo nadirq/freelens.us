2bafa5dc7c18a0ef133afbe155a417bca125b715
Nadir Nigmatullin <tatarin9105@mail.ru>
2014-08-06 16:14:07 +0700

    Map with geolocation

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,21 +1,66 @@
+<script src="//yandex.st/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+<script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+<script type="text/javascript">
+    ymaps.ready(function () {
+        var map;
+        ymaps.geolocation.get().then(function (res) {
+            var mapContainer = $('#map'),
+                bounds = res.geoObjects.get(0).properties.get('boundedBy'),
+            // Рассчитываем видимую область для текущей положения пользователя.
+                mapState = ymaps.util.bounds.getCenterAndZoom(
+                    bounds,
+                    [mapContainer.width(), mapContainer.height()]
+                );
+            createMap(mapState);
+        }, function (e) {
+            // Если место положение невозможно получить, то просто создаем карту.
+            createMap({
+                center: [55.751574, 37.573856],
+                zoom: 2
+            });
+        });
+
+        function createMap (state) {
+            state.zoom = 12;
+            state.controls = ['zoomControl'];
+            map = new ymaps.Map('map', state);
+        }
+    });
+
+
+
+
+
+
+
+
+
+</script>
+
 <?php
 /* @var $this SiteController */
-
 $this->pageTitle=Yii::app()->name;
 ?>
-<iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.ru/maps?ie=UTF8&amp;ll=55.031638,82.921715&amp;spn=0.024276,0.063643&amp;t=m&amp;z=15&amp;output=embed"></iframe><br />
+<h1>Welcome!</h1>
+<!--<p>Этот сервис предназначен для поиска фотографов и интересных мест для съемки.-->
+<!--    На карте вы можете выбрать интересные места и заказать съемку именно в этом месте. Также можете указать свое место, где бы вы хотели пофотографироваться. </p>-->
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<div id="map"></div>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+
+
+
+
+
+
+
+
+
+
+
+<!--<iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.ru/maps?ie=UTF8&amp;ll=55.031638,82.921715&amp;spn=0.024276,0.063643&amp;t=m&amp;z=15&amp;output=embed"></iframe><br />-->
+
+
+

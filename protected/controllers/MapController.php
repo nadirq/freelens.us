e7840438f7cd07a9588d119ae104470e55782197
Maksim Bondarenko <mbondarenko92@gmail.com>
2014-08-14 12:21:52 +0700

    Rules map

--- protected/controllers/MapController.php
+++ protected/controllers/MapController.php
@@ -2,30 +2,34 @@
 
 class MapController extends Controller
 {
+
+
+    public function filters()
+    {
+        return array(
+            'accessControl'
+        );
+    }
+
     public function actionIndex()
     {
         $this->render('index');
     }
 
-
     public function accessRules()
     {
         return array(
-            array('deny',
-                'actions'=>array('newPlacemark'),
-                'users'=>('?'),
-            ),
-            array('deny',
-                'actions'=>array('newPlacemark'),
-                'roles'=>('user'),
-            ),
-            array('allow',
-                'actions'=>array('showPlaces'),
-                'users'=>array('?'),
-            ),
             array('allow',
                 'actions'=>array('newPlacemark', 'upload'),
                 'roles'=>array('camerist'),
+            ),
+            array('allow',
+                'actions'=>array('showPlaces'),
+                'users'=>array('*'),
+            ),
+            array('deny',
+                'actions'=>array('newPlacemark', 'upload'),
+                'users'=>array('*'),
             ),
         );
     }
@@ -37,6 +41,7 @@ class MapController extends Controller
      */
     public function actionNewPlacemark()
     {
+        var_dump('jkgjkh'); exit();
         $album = Albums::model()->getAlbumId(Yii::app()->user->id);
         $id = Yii::app()->user->id;
         $this->render('newPlacemark', array('id' => $id, 'album' => $album->getPhotos()));
